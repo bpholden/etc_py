7116e15d079afc7ea55d7dc0c6a36bcf8c216e3b
Bradford Holden <brad.p.holden@gmail.com>
2019-12-22 13:47:34 -0500

    new functions

--- Telescope.py
+++ Telescope.py
@@ -14,5 +14,21 @@ class Telescope():
         self.plate_scale= 0. 
 
     def __repr__(self):
-        "<telescope %s %f>" % (self.name,self.area)
+        "<telescope %s %.1f %.1f>" % (self.name,self.area,self.plate_scale)
+
+
+    def keck(self):
+        self.name = 'Keck'
+        self.area = 723674.
+        self.plate_scale = 1.379
+
+    def shane(self):
+        self.name = 'Shane'
+        self.area = 63617.
+        self.plate_scale = 1.379
+
+    def APF(self):
+        self.name = 'APF'
+        self.area = 42053.
+        self.plate_scale = 5.8241
 
