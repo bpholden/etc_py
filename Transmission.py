195c0f8f899fea1befad0c465378c2c02435dc66
Bradford Holden <brad.p.holden@gmail.com>
2019-12-22 13:47:08 -0500

    new!

---
+++ Transmission.py
@@ -0,0 +1,44 @@
+import os
+import os.path
+
+import scipy
+import astropy
+import astropy.io.ascii
+import astropy.io.fits
+import numpy
+
+class Transmission():
+    def __init__(self,site=""):
+        self.site= site
+        self.inwave = None
+        self.inextinc = None
+        self.infile = self.extfilename()
+
+    def extfilename(self):
+        if self.site == "MH":
+            return "mthamextinct.dat"
+        elif self.site == "MK":
+            return "mkoextinct.dat"
+        else:
+            return ""
+        
+
+    def __repr__(self):
+        "<obs %s>" % (self.site)
+
+
+    def readin_ext(self)
+
+        try:
+            inpath = os.path.realpath(__file__)
+        else:
+            inpath = os.getcwd()
+        infile = os.path.join(inpath,"data",self.infile)
+        mtham_ext = astropy.io.ascii.read(inpath)
+        self.inwave = mtham_ext['col1']
+        self.inextinc = mtham_ext['col2']
+
+    def trans(self,wavegrid):
+        extgrid = numpy.interp(wavegrid,self.inwave,self.inextinc)
+        return extgrid
+    
