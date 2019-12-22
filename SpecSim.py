b7e3d643ae19f774f488e66f42cf30a63e2d6511
Bradford Holden <brad.p.holden@gmail.com>
2019-12-22 15:48:29 -0500

    check existence

--- SpecSim.py
+++ SpecSim.py
@@ -6,10 +6,15 @@ class SpecSim()
 
 
         
-    def calcS2N(self,instrument,obs,transmission):
+    def calcS2N(self,instrument=None,obs=None,transmission=None):
+
+        if instrument is not None:
+            self.instrument = instrument
+        if obs is not None:
+            self.obs = obs
+        if transmission is not None:
+            self.transmission = transmission
+
         
-        self.instrument = instrument
-        self.obs = obs
-        self.transmission = transmission
 
         
