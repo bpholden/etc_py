283e3ea3a94edf95fc2d9492d7a6ea12c3489010
Brad Holden <brad.p.holden@gmail.com>
2024-07-17 17:13:11 -0700

    add self

--- SpecSim.py
+++ SpecSim.py
@@ -6,7 +6,7 @@ class SpecSim()
 
 
         
-    def calcS2N(self,instrument=[],obs=None,transmission=None):
+    def calc_sn(self,instrument=[],obs=None,transmission=None):
 
         if instrument is not None:
             self.instrument = instrument
@@ -19,11 +19,11 @@ class SpecSim()
             return None
 
 
-        obs.normalize_template()
-        obs.compute_photons()
+        self.obs.normalize_template()
+        self.obs.compute_photons()
 
         # this generates the number of photons per A per cm^2 landing on the telescope
         # now need to take into account 
 
         if len(self.instrument) > 0:
-            obs.phot *= self.instrument[0].telescope.area
+            self.obs.phot *= self.instrument[0].telescope.area
